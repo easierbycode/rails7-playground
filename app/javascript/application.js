916d5f81fe24141b28973a654e4b5006bc1711a5
easierbycode <daniel@easierbycode.com>
2021-08-25 17:19:47 -0700

    initial commit (stimulus working, active_text unable to store images)

---
+++ app/javascript/application.js
@@ -0,0 +1,10 @@
+// Configure your import map in config/importmap.rb
+
+// import "@rails/actioncable"
+// import "@rails/activestorage"
+
+import "trix"
+import "@rails/actiontext"
+
+import '@hotwired/stimulus-importmap-autoloader'
+import '@hotwired/turbo-rails'
